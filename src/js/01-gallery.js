8fe05ad3b3bf0b17ec882d2014c846c461a5e23a
Andrii Nahornyi <navamster@gmail.com>
2022-09-13 18:03:30 +0300

    adds 01-gallery solutions

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery');
+const markupGallery = createGalleryItemsMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", markupGallery);
+
+// Listeners
+galleryEl.addEventListener('click', targetClickHandler);
+
+function targetClickHandler(event) {
+    event.preventDefault();
+}
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData:	'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+ 
+// 1. Create & render markup
+function createGalleryItemsMarkup(items) {
+    const galleryItemsMarkup = items.map(({ preview, original, description }) => `<a class="gallery__item" href='${original}'>
+                <img class='gallery__image'
+                    src='${preview}'
+                    alt='${description}'
+                />
+            </a>`
+    ).join('');
+    console.log(galleryItemsMarkup);
+    return galleryItemsMarkup;
+}  
 // Change code below this line
 
-console.log(galleryItems);
+
