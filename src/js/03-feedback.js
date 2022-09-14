81d12af5ceb1bc259751f5760c5dcbbe722a36ab
Andrii Nahornyi <navamster@gmail.com>
2022-09-14 16:04:10 +0300

    adds 03-feedback.js solutions

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import Throttle from "lodash.throttle";
+// Variables
+const form = document.querySelector(".feedback-form");
+const email = form.querySelector("[name='email']");
+const message = form.querySelector("[name='message']");
 
+const localKey = 'feedback-form-state';
+// Listeners
+form.addEventListener('input', Throttle(storageFormData, 500));
+form.addEventListener('submit', onFormSubmit);
+// Functions
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { email, message } = event.currentTarget.elements;
+    console.dir({ email: email.value, message: message.value });
+    // Alternative
+    // const savedData = JSON.parse(localStorage.getItem(localKey));
+    // console.dir(savedData);
+    localStorage.removeItem(localKey);
+    event.currentTarget.reset();
+}
+function storageFormData(event) {
+    const formValue = { email: '', message: '' };
+    if (localStorage.getItem(localKey)) {
+        Object.assign(formValue, JSON.parse(localStorage.getItem(localKey)));
+    }
+    // console.dir(event.target.value);
+    // console.log('name input:', event.target.name);
+    // console.log('value input:', event.target.value);
+    formValue[event.target.name] = event.target.value;
+    // console.log(formValue);
+    localStorage.setItem(localKey, JSON.stringify(formValue));
+}
